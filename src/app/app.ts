acd0bce1d779ac27b99d442513e86fedd697dc0b
Alejandro <alejandro_gonzalo92@icloud.com>
2026-04-13 23:51:00 +0200

    segundo commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,48 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('calculadoraDos');
+  display:string = '';
+
+  private firstValue: number | null = null;
+  private operator: string | null = null;
+
+  //Agregar numeros a la pantalla
+  append(value: string){
+    this.display += value;
+  }
+
+  //Elegir operacion
+  chooseOp(op: string){
+    this.firstValue = Number(this.display);
+    this.operator = op;
+    this.display = '';
+  }
+
+  //Caluclar resultado
+  calculate(){
+    const [a, op, b] = this.display.split(/([+\-*/])/);
+    this.firstValue = Number(a);
+    this.operator = op;
+    const secondValue = Number(b);
+    if( this.firstValue === null || this.operator === null) return;
+    let result:number = 0;
+
+    switch( this.operator){
+      case '+': result = this.firstValue + secondValue; break;
+      case '-': result = this.firstValue - secondValue; break;
+      case '*': result = this.firstValue * secondValue; break;
+      case '/': result = secondValue === 0 ? 0 : this.firstValue / secondValue; break;
+    }
+
+    this.display = result.toString();
+    this.firstValue = null;
+    this.operator = null;
+  }
+
+  //limpiar todo
+  clear(){
+    this.display = '';
+    this.firstValue = null;
+    this.operator = null;
+  }
 }
